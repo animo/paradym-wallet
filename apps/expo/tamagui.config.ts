edfd063c8bac5af3d9333bbe1a9585176b6a4d38
Timo Glastra <timo@animo.id>
2023-05-09 21:37:13 +0200

    chore: eslint setup
    Signed-off-by: Timo Glastra <timo@animo.id>

--- apps/expo/tamagui.config.ts
+++ apps/expo/tamagui.config.ts
@@ -6,6 +6,7 @@ import { config } from '@internal/ui'
 type Conf = typeof config
 
 declare module 'tamagui' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface TamaguiCustomConfig extends Conf {}
 }
 
--- packages/app/tamagui.config.ts
+++ packages/app/tamagui.config.ts
@@ -3,6 +3,7 @@ import { config } from '@internal/ui'
 export type Conf = typeof config
 
 declare module 'tamagui' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface TamaguiCustomConfig extends Conf {}
 }
 
