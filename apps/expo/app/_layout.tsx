996e2eca4bc78e31fe2bfac5fd3bde785ef05b84
Tom Lanser <tom@devv.nl>
2023-06-16 10:17:47 +0200

    feat: Added secure store for the wallet key (#21)
    Signed-off-by: Tom Lanser <tom@devv.nl>

--- apps/expo/app/_layout.tsx
+++ apps/expo/app/_layout.tsx
@@ -1,6 +1,7 @@
 import type { AppAgent } from '@internal/agent'
 
 import { AgentProvider, initializeAgent } from '@internal/agent'
+import { Heading, Page, Paragraph, useToastController, YStack } from '@internal/ui'
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { Provider } from 'app/provider'
 import { useFonts } from 'expo-font'
@@ -8,10 +9,12 @@ import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
 import { useEffect, useState } from 'react'
 
+import { getSecureWalletKey } from '../utils/walletKeyStore'
+
 void SplashScreen.preventAutoHideAsync()
 
 export default function HomeLayout() {
-  const [loaded] = useFonts({
+  const [fontLoaded] = useFonts({
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     InterRegular: require('@tamagui/font-inter/otf/Inter-Regular.otf'),
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
@@ -22,21 +25,55 @@ export default function HomeLayout() {
     InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
   })
   const [agent, setAgent] = useState<AppAgent>()
+  const [agentInitialisationFailed, setAgentInitialisationFailed] = useState(false)
+  const toast = useToastController()
 
   // Initialize agent
   useEffect(() => {
     if (agent) return
 
-    void initializeAgent().then((agent) => setAgent(agent))
+    const startAgent = async () => {
+      const walletKey = await getSecureWalletKey().catch(() => {
+        toast.show('Could not load wallet key from secure storage.')
+        setAgentInitialisationFailed(true)
+      })
+      if (!walletKey) return
+
+      const agent = await initializeAgent(walletKey).catch(() => {
+        toast.show('Could not initialize agent.')
+        setAgentInitialisationFailed(true)
+      })
+      if (!agent) return
+
+      setAgent(agent)
+    }
+
+    void startAgent()
   }, [])
 
-  // Hide splash screen when agent and fonts are loaded
+  // Hide splash screen when agent and fonts are loaded or agent could not be initialized
   useEffect(() => {
-    if (loaded && agent) void SplashScreen.hideAsync()
-  }, [loaded, agent])
+    if (fontLoaded && (agent || agentInitialisationFailed)) void SplashScreen.hideAsync()
+  }, [fontLoaded, agent, agentInitialisationFailed])
+
+  // Show error screen if agent could not be initialized
+  if (fontLoaded && agentInitialisationFailed) {
+    return (
+      <Provider>
+        <Page jc="center" ai="center" g="md">
+          <YStack>
+            <Heading variant="h1">Error</Heading>
+            <Paragraph>
+              Could not establish a secure environment. The current device could be not supported.
+            </Paragraph>
+          </YStack>
+        </Page>
+      </Provider>
+    )
+  }
 
   // The splash screen will be rendered on top of this
-  if (!loaded || !agent) {
+  if (!fontLoaded || !agent) {
     return null
   }
 
