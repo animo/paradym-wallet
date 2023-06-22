88d2a5cbbd5faf4e3259dbd2268ded084d202d41
Timo Glastra <timo@animo.id>
2023-06-22 12:27:21 +0200

    Update packages/app/components/CredentialCard.tsx

--- packages/app/components/CredentialCard.tsx
+++ packages/app/components/CredentialCard.tsx
@@ -63,7 +63,7 @@ export default function CredentialCard({
         borderWidth={0.5}
         borderColor="$borderTranslucent"
         onPress={onPress}
-        opacity={backgroundImage ? (isBackgroundImageLoaded ? 1 : 0) : 1}
+        opacity={backgroundImage && isBackgroundImageLoaded ? 1 : 0}
       >
         <Card.Header>
           <XStack jc="space-between">
