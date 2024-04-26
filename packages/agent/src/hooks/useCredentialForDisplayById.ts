3266afffabb097a816cfcefd6c38e04106849d3b
Timo Glastra <timo@animo.id>
2024-04-26 14:50:26 +0200

    fixes
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/agent/src/hooks/useCredentialForDisplayById.ts
+++ packages/agent/src/hooks/useCredentialForDisplayById.ts
@@ -1,20 +1,10 @@
-import { useCredentialById as _useCredentialById } from '@credo-ts/react-hooks'
-
-import { getCredentialExchangeForDisplay, getCredentialForDisplay } from '../display'
+import { getCredentialForDisplay } from '../display'
 import { useSdJwtVcRecordById, useW3cCredentialRecordById } from '../providers'
 
-export type CredentialForDisplayId =
-  | `credential-exchange-${string}`
-  | `w3c-credential-${string}`
-  | `sd-jwt-vc-${string}`
+export type CredentialForDisplayId = `w3c-credential-${string}` | `sd-jwt-vc-${string}`
 
 export const useCredentialForDisplayById = (credentialId: CredentialForDisplayId) => {
-  if (credentialId.startsWith('credential-exchange-')) {
-    const c = _useCredentialById(credentialId.replace('credential-exchange-', ''))
-    if (!c) return null
-
-    return getCredentialExchangeForDisplay(c)
-  } else if (credentialId.startsWith('w3c-credential-')) {
+  if (credentialId.startsWith('w3c-credential-')) {
     const c = useW3cCredentialRecordById(credentialId.replace('w3c-credential-', ''))
     if (!c) return null
 
