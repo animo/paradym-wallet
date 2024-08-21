2e0b5b0e9c9029c0169400f1d0998a81839f14b3
Jan <hey@janrietveld.com>
2024-08-21 17:37:37 +0200

    chore: idea comment

--- apps/ausweis/src/features/onboarding/screens/id-card-verify.tsx
+++ apps/ausweis/src/features/onboarding/screens/id-card-verify.tsx
@@ -7,6 +7,8 @@ export interface OnboardingIdCardFetchProps {
   goToNextStep: () => void
 }
 
+// We could also skip this screen and request biometrics in the flow like now, and only go to this screen if that fails.
+
 export function OnboardingIdCardVerify({ goToNextStep }: OnboardingIdCardFetchProps) {
   return (
     <YStack jc="space-between" fg={1}>
