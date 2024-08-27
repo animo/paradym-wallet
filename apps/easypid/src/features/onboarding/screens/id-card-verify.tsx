de5b935ca4df963a3928daa1d5dfdad26202bcf5
Timo Glastra <timo@animo.id>
2024-08-27 16:48:59 +0200

    fix
    Signed-off-by: Timo Glastra <timo@animo.id>

--- apps/easypid/src/features/onboarding/screens/id-card-verify.tsx
+++ apps/easypid/src/features/onboarding/screens/id-card-verify.tsx
@@ -1,18 +1,28 @@
-import { Button, IdCard, YStack } from '@package/ui'
-import React from 'react'
+import { Button, IdCard, Spinner, YStack } from '@package/ui'
+import React, { useState } from 'react'
 
 import germanIssuerImage from '../../../../assets/german-issuer-image.png'
 
 export interface OnboardingIdCardVerifyProps {
-  goToNextStep: () => void
+  goToNextStep: () => Promise<void>
 }
 
 export function OnboardingIdCardVerify({ goToNextStep }: OnboardingIdCardVerifyProps) {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const onUnlockWithBiometrics = () => {
+    if (isLoading) return
+
+    setIsLoading(true)
+    goToNextStep().finally(() => {
+      setIsLoading(false)
+    })
+  }
   return (
     <YStack jc="space-between" fg={1}>
       <IdCard hideUserName icon="biometric" issuerImage={germanIssuerImage} />
-      <Button.Solid scaleOnPress onPress={goToNextStep}>
-        Unlock with biometrics
+      <Button.Solid scaleOnPress onPress={onUnlockWithBiometrics} disabled={isLoading}>
+        {isLoading ? <Spinner variant="dark" /> : 'Unlock with biometrics'}
       </Button.Solid>
     </YStack>
   )
