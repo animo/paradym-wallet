5e80e035ddfdf22de4e79ed9a3cfbfb83d0dfea5
Jan <hey@janrietveld.com>
2025-01-13 14:52:42 +0100

    fix: rm flag

--- apps/easypid/src/hooks/useFeatureFlag.tsx
+++
@@ -1,7 +0,0 @@
-import { CURRENT_APP_TYPE } from '../config/appType'
-import { APP_CONFIGS } from '../config/features'
-import type { FeatureKey } from '../config/features'
-
-export const useFeatureFlag = (featureKey: FeatureKey) => {
-  return APP_CONFIGS[CURRENT_APP_TYPE]?.[featureKey] ?? false
-}
