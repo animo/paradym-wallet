24797ffdb42af128dff408b7d68efcaa0d975eac
Jan <hey@janrietveld.com>
2025-01-29 13:37:03 +0100

    feat: new settings start
    Signed-off-by: Jan <hey@janrietveld.com>

--- apps/easypid/src/features/menu/FunkeMenuScreen.tsx
+++ apps/easypid/src/features/menu/FunkeMenuScreen.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import type React from 'react'
 
 import { useHaptics, useScrollViewPosition } from '@package/app/src/hooks'
 import {
+  AnimatedStack,
   Button,
   FlexPage,
   HeaderContainer,
   Heading,
   HeroIcons,
+  MessageBox,
   ScrollView,
   Stack,
   XStack,
@@ -17,7 +19,8 @@ import {
 import { usePidCredential } from '@easypid/hooks'
 import { useWalletReset } from '@easypid/hooks/useWalletReset'
 import { TextBackButton } from '@package/app'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
+import { cloneElement } from 'react'
 import { Linking } from 'react-native'
 import Animated from 'react-native-reanimated'
 
@@ -49,6 +52,52 @@ const menuItems = [
   },
 ]
 
+// Create new menu item component
+// with props:
+// - variant: 'default' | 'danger'
+// - onPress
+// - icon
+// - label
+
+type MenuListItemProps = {
+  variant?: 'default' | 'danger'
+  onPress: () => void
+  icon: React.ReactElement
+  label: string
+  action?: 'outside' | 'info' | 'route' | 'none'
+}
+
+export const MenuListItem = ({ variant = 'default', onPress, icon, label, action = 'route' }: MenuListItemProps) => {
+  const { pressStyle, handlePressIn, handlePressOut } = useScaleAnimation()
+  const { withHaptics } = useHaptics()
+
+  return (
+    <AnimatedStack
+      style={pressStyle}
+      onPressIn={handlePressIn}
+      onPressOut={handlePressOut}
+      onPress={withHaptics(onPress)}
+      flexDirection="row"
+      ai="center"
+      jc="space-between"
+      gap="$4"
+      px="$4"
+      py="$2"
+    >
+      <XStack ai="center" gap="$4">
+        <Stack p="$3" bg={variant === 'default' ? '$grey-50' : '$danger-300'} br="$6">
+          {cloneElement(icon, { color: variant === 'default' ? '$grey-900' : '$danger-600' })}
+        </Stack>
+        <Heading variant="h3" fontWeight="$semiBold" color={variant === 'default' ? '$grey-900' : '$danger-600'}>
+          {label}
+        </Heading>
+      </XStack>
+      {action === 'route' && <HeroIcons.ChevronRight color="$grey-500" />}
+      {action === 'info' && <HeroIcons.InformationCircleFilled color="$grey-500" />}
+      {action === 'outside' && <HeroIcons.ArrowUpRightFilled color="$grey-500" />}
+    </AnimatedStack>
+  )
+}
 export function FunkeMenuScreen() {
   const { handleScroll, isScrolledByOffset, scrollEventThrottle } = useScrollViewPosition()
   const onResetWallet = useWalletReset()
@@ -80,24 +129,52 @@ export function FunkeMenuScreen() {
     <FlexPage gap="$0" paddingHorizontal="$0">
       <HeaderContainer isScrolledByOffset={isScrolledByOffset} title="Menu" />
       <ScrollView onScroll={handleScroll} scrollEventThrottle={scrollEventThrottle}>
-        <YStack fg={1} jc="space-between">
-          <YStack>
-            {idItem}
-            {menuItems.map((item, idx) => (
-              <MenuItem key={item.title} item={item} idx={idx} />
-            ))}
-            <YStack py="$4" ai="center">
-              <YStack px="$4" w="60%">
-                <Button.Solid
-                  accessible={true}
-                  accessibilityRole="button"
-                  aria-label="Reset wallet"
-                  scaleOnPress
-                  onPress={onResetWallet}
-                >
-                  Reset wallet
-                </Button.Solid>
-              </YStack>
+        <YStack fg={1} gap="$6" jc="space-between">
+          {!credential && (
+            <Stack px="$4">
+              <MessageBox
+                variant="info"
+                icon={<HeroIcons.ArrowRight />}
+                title="Setup digital ID"
+                message="Use your eID card to set up your digital identity."
+                onPress={() => router.push('/pidSetup')}
+              />
+            </Stack>
+          )}
+          <YStack gap="$3">
+            <Heading px="$4" variant="sub2" fontWeight="$semiBold">
+              WALLET
+            </Heading>
+            <YStack>
+              <MenuListItem onPress={() => undefined} icon={<HeroIcons.CreditCardFilled />} label="Cards" />
+              <MenuListItem onPress={() => undefined} icon={<HeroIcons.QueueListFilled />} label="Activity" />
+            </YStack>
+          </YStack>
+          <YStack gap="$3">
+            <Heading px="$4" variant="sub2" fontWeight="$semiBold">
+              APP
+            </Heading>
+            <YStack>
+              <MenuListItem onPress={() => undefined} icon={<HeroIcons.Cog8ToothFilled />} label="Settings" />
+              <MenuListItem
+                onPress={() => undefined}
+                icon={<HeroIcons.ChatBubbleBottomCenterTextFilled />}
+                label="Feedback"
+                action="outside"
+              />
+              <MenuListItem
+                onPress={() => undefined}
+                icon={<HeroIcons.InformationCircleFilled />}
+                label="About the wallet"
+                action="none"
+              />
+              <MenuListItem
+                variant="danger"
+                onPress={() => undefined}
+                icon={<HeroIcons.TrashFilled />}
+                label="Reset wallet"
+                action="none"
+              />
             </YStack>
           </YStack>
         </YStack>
