f54fcafdae950428a1855ed51d8a0d5ad2e831fc
Timo Glastra <timo@animo.id>
2025-08-26 17:48:39 +0200

    fix: use debounce in the toast for no internet
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/app/src/hooks/useHasInternetConnection.tsx
+++ packages/app/src/hooks/useHasInternetConnection.tsx
@@ -1,16 +1,11 @@
 import { useNetInfo } from '@react-native-community/netinfo'
-import { useDebounce } from './useDebounce'
 
 export { fetch as getNetInfo } from '@react-native-community/netinfo'
 
 export const useHasInternetConnection = () => {
   const { isConnected, isInternetReachable } = useNetInfo()
 
-  // Delay whether the device has internet by 5 seconds, since it sometimes returns
-  // false after being in the background for a while.
-  const hasInternetConnection = useDebounce((isConnected && isInternetReachable) ?? false, 5000)
-
-  return hasInternetConnection
+  return (isConnected && isInternetReachable) ?? false
 }
 
 export const useIsConnectedToWifi = () => {
